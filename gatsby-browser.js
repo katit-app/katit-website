bd4649b68bacf3de3c669eef73a64a609e8ce52a
unknown <n.roy@paralect.com>
2024-01-24 21:20:16 +0100

    kek

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,9 +4,9 @@ import { NotificationProvider } from './src/context/AddItemNotificationProvider'
 import { AuthContextProvider } from './src/context/AuthProvider';
 
 export const wrapRootElement = ({ element }) => (
-  <NotificationProvider>
-    <AuthContextProvider>
+  <AuthContextProvider>
+    <NotificationProvider>
       {element}
-    </AuthContextProvider>
-  </NotificationProvider>
+    </NotificationProvider>
+  </AuthContextProvider>
 );
