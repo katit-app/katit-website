4aabf88b5ef2cf4a7c82a3e59ca7e86af6abf906
unknown <n.roy@paralect.com>
2024-08-21 18:47:19 +0200

    update info and soldout products

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -28,25 +28,74 @@ const Contact = (props) => {
   return (
     <div className={styles.root}>
       <div className={styles.section}>
-        <h4>Send Us A Message</h4>
+        <h4>HOW TO CONTACT US</h4>
         <p>
-          Our Customer Service team are here for all enquiries Monday to Friday,
-          9am - 5pm CET.
+          You can send us an e-mail to katit.store@gmail.com or write us a message on our instagram account @katit.store
         </p>
-        <p>We look forward to hearing from you.</p>
+        <p>
+          You must describe the problem you have had with the order (size, damaged product...) and tell us your name and order number.
+        </p>
+        <p>Our solutions:</p>
+        <ol>
+          <li>
+            <p>Responsibility of KATIT</p>
+            <ul>
+              <li>
+                If our company is responsible for the issue, we will allow a return to our warehouse for a refund or exchange.
+              </li>
+              <li>
+                Upon receipt of the item, we will either fully refund the original price of the product or send you a free replacement product at no additional cost. This solution is only applicable when KATIT is responsible for shipping the incorrect item or size.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>Responsibility of the client</p>
+            <ul>
+              <li>
+                Incorrect order (item size):
+                <p>
+                  If the customer has ordered an incorrect item or size, we may also allow a return. In this case, the customer will be responsible for shipping costs in both directions (to and from our warehouse) for an exchange. <u>No shipping costs are refundable.</u>
+                </p>
+              </li>
+              <li>
+                Unwanted item:
+                <p>
+                  If you decide that you no longer want the item or simply want to exchange it, you can inform us within 15 days from the date of receipt of the product. In this case the product must be sent back to our warehouse. Once we receive the product and check that it is in good condition we will proceed to make the refund.
+                </p>
+              </li>
+              <li>
+                Order Cancellation Policy:
+                <p>
+                  If you wish to cancel your order before it has been shipped, please contact us as soon as possible. We will make every effort to process your cancellation. However, if the order has already been processed for shipping, please note that shipping costs are non-refundable.
+                </p>
+              </li>
+              <li>
+                Refund:
+                <p>
+                  Once your return is received and inspected, we will send an email to notify you that we have received your return. 
+                </p>
+                <p>
+                  If the refund is approved, then it will be processed and a credit will be automatically applied to your credit card or original method of payment. This may take up to 10 business days.
+                </p>
+              </li>
+            </ul>
+          </li>
+        </ol>
       </div>
 
       <div className={styles.section}>
-        <h4>Phone</h4>
-        <p>+48 516 290 866</p>
-        <p>Monday to Friday - 9am - 5pm CET</p>
-      </div>
-
-      <div className={styles.section}>
-        <h4>Email</h4>
+        <h4>ADDITIONAL NOTES ON WARRANTY</h4>
+        <p>*All returns must first be approved by our customer service team. </p>
+        <br/>
         <p>
-          You can email us at katit.store@gmail.com
+          *All returned items must be in perfect condition, unworn, unwashed and must retain all original tags and packaging.
         </p>
+        <br/>
+        <p>
+          *No shipping costs will be refunded for the return of the item (unless it is the responsibility of KATIT).
+        </p>
+        <br/>
+        <p>Any questions please email us at katit.store@gmail.com</p>
       </div>
 
       {/* <div className={styles.contactContainer}>
--- src/components/Returns/Returns.js
+++ src/components/Returns/Returns.js
@@ -5,13 +5,34 @@ const Returns = (props) => {
   return (
     <div className={styles.root}>
       <div className={styles.section}>
-        <h3>Return details</h3>
+        <h3>Returns and Refunds</h3>
         <p>
-        You have 14 days from the date of receipt of your order to return any unwanted or unsuitable products. 
-        If you want to return items you may do so through a shipping method of your choice (this option is carried out at your own cost and responsibility).
+          At KATIT, customer satisfaction is our top priority. Whatever problems you have, we are here to help and will always look for a solution that is satisfactory to everyone.
         </p>
         <p>
-        Important! The products which you wish to return cannot bear any marks of usage and must have all the original tags attached.
+          All of our products go through quality control process before they are shipped to ensure that you receive the highest quality clothes. However, in the unlikely event that you receive a faulty or damaged item, we will take responsibility and offer you a return or exchange within 15 days of receiving your order.
+        </p>
+        <p>
+          If you have received a defective or damaged item, the wrong size or the wrong product, contact us at our email katit.store@gmail.com we will be at your service and we will answer all your requests in less than 24 hours.
+        </p>
+        <br/>
+        <p>
+          Our return policy is 15 days, so if more than 15 days have passed from the date of receipt of the item unfortunately we will not be able to offer any refund or exchange.
+        </p>
+        <ol>
+          <li>
+            Free Returns: We offer free returns to most countries. You can check if your country is eligible for free returns by contacting us before placing your order.
+          </li>
+          <li>
+            Buyer Responsibility: If your country isn't eligible for free returns, you're responsible for covering the return shipping costs. Additionally, any customs duties and taxes incurred during the return or shipping process are also responsibility of the client.
+          </li>
+          <li>
+            Multiple Item Returns: If you order multiple items and decide to return all of them, a fee may be deducted from your total refund to cover the costs associated with processing your specific order.
+          </li>
+        </ol>
+        <br/>
+        <p>
+          Please keep in mind that we are a small business when placing an order. It is important to order responsibly to avoid unnecessary extra costs.
         </p>
       </div>
     </div>
